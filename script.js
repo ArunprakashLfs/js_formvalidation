f2b70bc28ab11ab505d3a2bbc83248227ce255a8
kesavan <arunprakash7871@gmail.com>
2023-03-02 02:57:29 -0500

    add eventlistner

--- script.js
+++ script.js
@@ -5,12 +5,23 @@ const userName = document.getElementById("username");
 const Email = document.getElementById("email");
 const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirmpassword");
+const small = document.getElementById("outputvalue");
+const submit = document.getElementById("submit");
 
 
 //function
 
 
 //Event Listner
-form.addEventListener("submit", (event) => {
+submit.addEventListener ("submit", function (event) {
     event.preventDefault();
+    const userName = userName.value.trim();
+    const email = email.value.trim();
+    const password = password.value.trim();
+    const confirmpassword = confirmpassword.value.trim();
+    if (!userName) {
+        small.textContent = "enter proper value";
+    }else{
+        small.textContent = "success";
+    }
 })
