3ac7027677b22e0f39ed371f4e6254d261bd2224
kesavan <arunprakash7871@gmail.com>
2023-03-02 02:22:25 -0500

    add script in this file

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict'
+// get input element
+const userName = document.getElementById("username");
+const Email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmpassword");
+
+
+//function
+
+
+//Event Listner
