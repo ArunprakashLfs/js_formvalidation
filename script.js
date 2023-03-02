eefdff28b65fc6f49b0b3ddb549ecc9ff6e3d08a
kesavan <arunprakash7871@gmail.com>
2023-03-02 02:29:45 -0500

    addEventlistner in js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict'
 // get input element
+const form = document.getElementById("form");
 const userName = document.getElementById("username");
 const Email = document.getElementById("email");
 const password = document.getElementById("password");
@@ -10,3 +11,6 @@ const confirmPassword = document.getElementById("confirmpassword");
 
 
 //Event Listner
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+})
