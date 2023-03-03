d9b049a8484c0ccf0744369312a662be64da1816
kesavan <arunprakash7871@gmail.com>
2023-03-03 00:46:49 -0500

    get input from html

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
 'use strict'
-// get input element
-const form = document.getElementById("form");
-const userName = document.getElementById("username");
-const Email = document.getElementById("email");
-const password = document.getElementById("password");
-const confirmPassword = document.getElementById("confirmpassword");
-const small = document.getElementById("outputvalue");
-const submit = document.getElementById("submit");
 
-
-//function
-
-
-//Event Listner
-submit.addEventListener ("submit", function (event) {
-    event.preventDefault();
-    const userName = userName.value.trim();
-    const email = email.value.trim();
-    const password = password.value.trim();
-    const confirmpassword = confirmpassword.value.trim();
-    if (!userName) {
-        small.textContent = "enter proper value";
-    }else{
-        small.textContent = "success";
-    }
-})
+//getting input from html
+const formEl = document.getElementById("form");
+const usernameEl = document.getElementById("username");
+const passwordEl = document.getElementById("password");
+const confirmpasswordEl = document.getElementById("confirmpassword");
