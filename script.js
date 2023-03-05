8cf8955756ce717249a7a4b81496d71d211bb7f1
ArunprakashLfs <arunprakash7871@gmail.com>
2023-03-05 18:53:17 +0530

    added js and css properties

--- script.js
+++ script.js
@@ -8,9 +8,62 @@ const confirmpasswordEl = document.getElementById("confirmpassword");
 const emailEl = document.getElementById("email");
 
 //functions
+//error function
+const errorFunction = (element, message) => {
+    let formControl = element.parentElement;
+    // console.log(formControl);
+    formControl.className = "form-control error";
+    let small = formControl.querySelector(".small");
+    // console.log(small);
+    small.innerText = `${message} is invalid!`;
+  };
+  
+  //success function
+  const successFunction = (element) => {
+    let formControl = element.parentElement;
+    formControl.className = "form-control success";
+  };
 
 //Event listners
 formEl.addEventListener("submit", (e) => {
     e.preventDefault();
+    let username = usernameEl.value.trim();
+    let email = emailEl.value.trim();
+    let password = passwordEl.value.trim();
+    let confirmpassword = confirmpasswordEl.value.trim();
 
-})
+    checkRequired(input);
+    if (userName) {
+        successFunction(usernameEl);
+      } else {
+        errorFunction(usernameEl, "Username");
+      }
+      if (email) {
+        successFunction(emailEl);
+      } else {
+        errorFunction(emailEl, "E-mail");
+      }
+      if (password) {
+        successFunction(passwordEl);
+      } else {
+        errorFunction(passwordEl, "Password");
+      }
+      if (confirmpassword) {
+        //password === confirm password
+        if (password === confirmpassword) {
+          successFunction(confirmpasswordEl);
+        } else {
+          errorFunction(
+            confirmpasswordEl,
+            "confirm-password not matched to previous one so it"
+          );
+        }
+      } else {
+        errorFunction(confirmpasswordEl, "Confirm Password");
+      }
+    
+      // console.log(userName);
+      // console.log(email);
+      // console.log(password);
+      // console.log(confirmPassword);
+    });
