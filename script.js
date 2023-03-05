a87d225494bcbcd65303e88477998243257200d5
ArunprakashLfs <arunprakash7871@gmail.com>
2023-03-05 18:39:25 +0530

    add prevent default function

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const formEl = document.getElementById("form");
 const usernameEl = document.getElementById("username");
 const passwordEl = document.getElementById("password");
 const confirmpasswordEl = document.getElementById("confirmpassword");
+const emailEl = document.getElementById("email");
+
+//functions
+
+//Event listners
+formEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+})
